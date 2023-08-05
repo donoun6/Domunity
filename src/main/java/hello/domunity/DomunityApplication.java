4946841216e22c51b5612895f3ac1f5f17018a85
donoun6 <donoun6@naver.com>
2023-08-05 15:41:03 +0900

    css - 수정

--- src/main/java/hello/domunity/DomunityApplication.java
+++ src/main/java/hello/domunity/DomunityApplication.java
@@ -3,6 +3,7 @@ package hello.domunity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 //(exclude = SecurityAutoConfiguration.class)
 @SpringBootApplication
 public class DomunityApplication {
